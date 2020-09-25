942c62798053374106831fd568778f7d735fac2d
Cook Green <dell20091234@gmail.com>
2020-09-25 09:11:43 +0800

    Initial Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ArenaModdingTool
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPlugin = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuToolLanguage = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelControls = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileNewProject = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileNewOtherFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOpenProject = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOpenOtherFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuTool,
+            this.mnuPlugin,
+            this.mnuHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileNew,
+            this.mnuFileOpen,
+            this.toolStripMenuItem1,
+            this.mnuFileExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(39, 21);
+            this.mnuFile.Text = "File";
+            // 
+            // mnuPlugin
+            // 
+            this.mnuPlugin.Name = "mnuPlugin";
+            this.mnuPlugin.Size = new System.Drawing.Size(55, 21);
+            this.mnuPlugin.Text = "Plugin";
+            // 
+            // mnuHelp
+            // 
+            this.mnuHelp.Name = "mnuHelp";
+            this.mnuHelp.Size = new System.Drawing.Size(47, 21);
+            this.mnuHelp.Text = "Help";
+            // 
+            // mnuTool
+            // 
+            this.mnuTool.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuToolLanguage});
+            this.mnuTool.Name = "mnuTool";
+            this.mnuTool.Size = new System.Drawing.Size(46, 21);
+            this.mnuTool.Text = "Tool";
+            // 
+            // mnuToolLanguage
+            // 
+            this.mnuToolLanguage.Name = "mnuToolLanguage";
+            this.mnuToolLanguage.Size = new System.Drawing.Size(180, 22);
+            this.mnuToolLanguage.Text = "language";
+            // 
+            // panelControls
+            // 
+            this.panelControls.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelControls.Location = new System.Drawing.Point(0, 25);
+            this.panelControls.Name = "panelControls";
+            this.panelControls.Size = new System.Drawing.Size(142, 425);
+            this.panelControls.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(142, 25);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(658, 425);
+            this.panel2.TabIndex = 2;
+            // 
+            // mnuFileExit
+            // 
+            this.mnuFileExit.Name = "mnuFileExit";
+            this.mnuFileExit.Size = new System.Drawing.Size(180, 22);
+            this.mnuFileExit.Text = "Exit";
+            // 
+            // mnuFileNew
+            // 
+            this.mnuFileNew.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileNewProject,
+            this.mnuFileNewOtherFile});
+            this.mnuFileNew.Name = "mnuFileNew";
+            this.mnuFileNew.Size = new System.Drawing.Size(180, 22);
+            this.mnuFileNew.Text = "New";
+            // 
+            // mnuFileNewProject
+            // 
+            this.mnuFileNewProject.Name = "mnuFileNewProject";
+            this.mnuFileNewProject.Size = new System.Drawing.Size(180, 22);
+            this.mnuFileNewProject.Text = "Project";
+            // 
+            // mnuFileNewOtherFile
+            // 
+            this.mnuFileNewOtherFile.Name = "mnuFileNewOtherFile";
+            this.mnuFileNewOtherFile.Size = new System.Drawing.Size(180, 22);
+            this.mnuFileNewOtherFile.Text = "File";
+            // 
+            // mnuFileOpen
+            // 
+            this.mnuFileOpen.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileOpenProject,
+            this.mnuFileOpenOtherFile});
+            this.mnuFileOpen.Name = "mnuFileOpen";
+            this.mnuFileOpen.Size = new System.Drawing.Size(180, 22);
+            this.mnuFileOpen.Text = "Open";
+            // 
+            // mnuFileOpenProject
+            // 
+            this.mnuFileOpenProject.Name = "mnuFileOpenProject";
+            this.mnuFileOpenProject.Size = new System.Drawing.Size(180, 22);
+            this.mnuFileOpenProject.Text = "Project";
+            // 
+            // mnuFileOpenOtherFile
+            // 
+            this.mnuFileOpenOtherFile.Name = "mnuFileOpenOtherFile";
+            this.mnuFileOpenOtherFile.Size = new System.Drawing.Size(180, 22);
+            this.mnuFileOpenOtherFile.Text = "File";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelControls);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arena - Modding Tool for Bannerlord";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlugin;
+        private System.Windows.Forms.ToolStripMenuItem mnuHelp;
+        private System.Windows.Forms.ToolStripMenuItem mnuTool;
+        private System.Windows.Forms.ToolStripMenuItem mnuToolLanguage;
+        private System.Windows.Forms.Panel panelControls;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileNew;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileNewProject;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileNewOtherFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpenProject;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpenOtherFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArenaModdingTool
+{
+    public partial class frmMain : Form, ILocalization
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            SwitchLanguage();
+        }
+
+        public void SwitchLanguage()
+        {
+
+        }
+    }
+}
