f04b0024c614353d3c28dd98c24986e443eab9ec
Cook Green <dell20091234@gmail.com>
2021-09-06 21:05:08 +0800

    Add ItemComponentWeaponEditor

--- Forms/frmItemComponentWeaponEditor.Designer.cs
+++ Forms/frmItemComponentWeaponEditor.Designer.cs
@@ -29,12 +29,273 @@ namespace ArenaModdingTool.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtWeaponClass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWeaponBalance = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtThrustSpeed = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSpeedRating = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMissileSpeed = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtWeaponLength = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSwingDamage = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSwingDamageType = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtItemUsage = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtPhysicsMaterial = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtWeaponClass
+            // 
+            this.txtWeaponClass.Location = new System.Drawing.Point(140, 22);
+            this.txtWeaponClass.Name = "txtWeaponClass";
+            this.txtWeaponClass.Size = new System.Drawing.Size(305, 21);
+            this.txtWeaponClass.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Weapon Class:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Weapon Balance:";
+            // 
+            // txtWeaponBalance
+            // 
+            this.txtWeaponBalance.Location = new System.Drawing.Point(140, 61);
+            this.txtWeaponBalance.Name = "txtWeaponBalance";
+            this.txtWeaponBalance.Size = new System.Drawing.Size(305, 21);
+            this.txtWeaponBalance.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Thrust Speed:";
+            // 
+            // txtThrustSpeed
+            // 
+            this.txtThrustSpeed.Location = new System.Drawing.Point(140, 101);
+            this.txtThrustSpeed.Name = "txtThrustSpeed";
+            this.txtThrustSpeed.Size = new System.Drawing.Size(305, 21);
+            this.txtThrustSpeed.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Speed Rating:";
+            // 
+            // txtSpeedRating
+            // 
+            this.txtSpeedRating.Location = new System.Drawing.Point(140, 143);
+            this.txtSpeedRating.Name = "txtSpeedRating";
+            this.txtSpeedRating.Size = new System.Drawing.Size(305, 21);
+            this.txtSpeedRating.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Missile Speed:";
+            // 
+            // txtMissileSpeed
+            // 
+            this.txtMissileSpeed.Location = new System.Drawing.Point(140, 184);
+            this.txtMissileSpeed.Name = "txtMissileSpeed";
+            this.txtMissileSpeed.Size = new System.Drawing.Size(305, 21);
+            this.txtMissileSpeed.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Weapon Length:";
+            // 
+            // txtWeaponLength
+            // 
+            this.txtWeaponLength.Location = new System.Drawing.Point(140, 227);
+            this.txtWeaponLength.Name = "txtWeaponLength";
+            this.txtWeaponLength.Size = new System.Drawing.Size(305, 21);
+            this.txtWeaponLength.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 272);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Swing Damage:";
+            // 
+            // txtSwingDamage
+            // 
+            this.txtSwingDamage.Location = new System.Drawing.Point(140, 269);
+            this.txtSwingDamage.Name = "txtSwingDamage";
+            this.txtSwingDamage.Size = new System.Drawing.Size(305, 21);
+            this.txtSwingDamage.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 312);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 12);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Swing Damage Type:";
+            // 
+            // txtSwingDamageType
+            // 
+            this.txtSwingDamageType.Location = new System.Drawing.Point(140, 309);
+            this.txtSwingDamageType.Name = "txtSwingDamageType";
+            this.txtSwingDamageType.Size = new System.Drawing.Size(305, 21);
+            this.txtSwingDamageType.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(21, 349);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 12);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Item Usage:";
+            // 
+            // txtItemUsage
+            // 
+            this.txtItemUsage.Location = new System.Drawing.Point(140, 346);
+            this.txtItemUsage.Name = "txtItemUsage";
+            this.txtItemUsage.Size = new System.Drawing.Size(305, 21);
+            this.txtItemUsage.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(21, 388);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(107, 12);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Physics Material:";
+            // 
+            // txtPhysicsMaterial
+            // 
+            this.txtPhysicsMaterial.Location = new System.Drawing.Point(140, 385);
+            this.txtPhysicsMaterial.Name = "txtPhysicsMaterial";
+            this.txtPhysicsMaterial.Size = new System.Drawing.Size(305, 21);
+            this.txtPhysicsMaterial.TabIndex = 23;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(370, 415);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 24;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(289, 415);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 25;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // frmItemComponentWeaponEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmItemComponentWeaponEditor";
+            this.ClientSize = new System.Drawing.Size(469, 450);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtPhysicsMaterial);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtItemUsage);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtSwingDamageType);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtSwingDamage);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtWeaponLength);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMissileSpeed);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSpeedRating);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtThrustSpeed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWeaponBalance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtWeaponClass);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmItemComponentWeaponEditor";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Item Component - Weapon Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtWeaponClass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWeaponBalance;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtThrustSpeed;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSpeedRating;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMissileSpeed;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtWeaponLength;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSwingDamage;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtSwingDamageType;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtItemUsage;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtPhysicsMaterial;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
     }
 }
--- Forms/frmItemComponentWeaponEditor.cs
+++ Forms/frmItemComponentWeaponEditor.cs
@@ -23,6 +23,42 @@ namespace ArenaModdingTool.Forms
         {
             InitializeComponent();
             this.itemWeapon = itemWeapon;
+            if (itemWeapon != null)
+            {
+                txtItemUsage.Text = itemWeapon.item_usage;
+                txtMissileSpeed.Text = itemWeapon.missile_speed;
+                txtPhysicsMaterial.Text = itemWeapon.physics_material;
+                txtSpeedRating.Text = itemWeapon.speed_rating;
+                txtSwingDamage.Text = itemWeapon.swing_damage;
+                txtSwingDamageType.Text = itemWeapon.swing_damage_type;
+                txtThrustSpeed.Text = itemWeapon.thrust_speed;
+                txtWeaponBalance.Text = itemWeapon.weapon_balance;
+                txtWeaponClass.Text = itemWeapon.weapon_class;
+                txtWeaponLength.Text = itemWeapon.weapon_length;
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            itemWeapon = new MBItemWeapon();
+            itemWeapon.item_usage = txtItemUsage.Text;
+            itemWeapon.missile_speed = txtMissileSpeed.Text;
+            itemWeapon.physics_material = txtPhysicsMaterial.Text;
+            itemWeapon.speed_rating = txtSpeedRating.Text;
+            itemWeapon.swing_damage = txtSwingDamage.Text;
+            itemWeapon.swing_damage_type = txtSwingDamageType.Text;
+            itemWeapon.thrust_speed = txtThrustSpeed.Text;
+            itemWeapon.weapon_balance = txtWeaponBalance.Text;
+            itemWeapon.weapon_class = txtWeaponClass.Text;
+            itemWeapon.weapon_length = txtWeaponLength.Text;
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
