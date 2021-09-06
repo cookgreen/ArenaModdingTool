195d0f6a2b294d0e63a8df80042446e797e486e8
Cook Green <dell20091234@gmail.com>
2021-09-06 14:07:12 +0800

    Add ItemComponentArmourEditor

--- Forms/frmItemComponentArmourEditor.Designer.cs
+++ Forms/frmItemComponentArmourEditor.Designer.cs
@@ -29,12 +29,281 @@ namespace ArenaModdingTool.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHeadArmour = new System.Windows.Forms.TextBox();
+            this.txtHairCoverType = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBreadCoverType = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtModifierGroup = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMaterialType = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtLegArmour = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtArmArmour = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtBodyArmour = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.chkHasGenderVariations = new System.Windows.Forms.CheckBox();
+            this.chkCoversLegs = new System.Windows.Forms.CheckBox();
+            this.chkCoversHands = new System.Windows.Forms.CheckBox();
+            this.chkCoversBody = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Head Armour:";
+            // 
+            // txtHeadArmour
+            // 
+            this.txtHeadArmour.Location = new System.Drawing.Point(168, 19);
+            this.txtHeadArmour.Name = "txtHeadArmour";
+            this.txtHeadArmour.Size = new System.Drawing.Size(281, 21);
+            this.txtHeadArmour.TabIndex = 3;
+            // 
+            // txtHairCoverType
+            // 
+            this.txtHairCoverType.Location = new System.Drawing.Point(168, 88);
+            this.txtHairCoverType.Name = "txtHairCoverType";
+            this.txtHairCoverType.Size = new System.Drawing.Size(281, 21);
+            this.txtHairCoverType.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hair Cover Type:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Bread Cover Type:";
+            // 
+            // txtBreadCoverType
+            // 
+            this.txtBreadCoverType.Location = new System.Drawing.Point(168, 124);
+            this.txtBreadCoverType.Name = "txtBreadCoverType";
+            this.txtBreadCoverType.Size = new System.Drawing.Size(281, 21);
+            this.txtBreadCoverType.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Modifier Group:";
+            // 
+            // txtModifierGroup
+            // 
+            this.txtModifierGroup.Location = new System.Drawing.Point(168, 161);
+            this.txtModifierGroup.Name = "txtModifierGroup";
+            this.txtModifierGroup.Size = new System.Drawing.Size(281, 21);
+            this.txtModifierGroup.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Material Type:";
+            // 
+            // txtMaterialType
+            // 
+            this.txtMaterialType.Location = new System.Drawing.Point(168, 201);
+            this.txtMaterialType.Name = "txtMaterialType";
+            this.txtMaterialType.Size = new System.Drawing.Size(281, 21);
+            this.txtMaterialType.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 245);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 12);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Leg Armour:";
+            // 
+            // txtLegArmour
+            // 
+            this.txtLegArmour.Location = new System.Drawing.Point(168, 242);
+            this.txtLegArmour.Name = "txtLegArmour";
+            this.txtLegArmour.Size = new System.Drawing.Size(281, 21);
+            this.txtLegArmour.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 328);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 12);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Arm Armour:";
+            // 
+            // txtArmArmour
+            // 
+            this.txtArmArmour.Location = new System.Drawing.Point(168, 325);
+            this.txtArmArmour.Name = "txtArmArmour";
+            this.txtArmArmour.Size = new System.Drawing.Size(281, 21);
+            this.txtArmArmour.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(25, 406);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(77, 12);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Body Armour:";
+            // 
+            // txtBodyArmour
+            // 
+            this.txtBodyArmour.Location = new System.Drawing.Point(168, 403);
+            this.txtBodyArmour.Name = "txtBodyArmour";
+            this.txtBodyArmour.Size = new System.Drawing.Size(281, 21);
+            this.txtBodyArmour.TabIndex = 21;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(374, 469);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 24;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(293, 469);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 25;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // chkHasGenderVariations
+            // 
+            this.chkHasGenderVariations.AutoSize = true;
+            this.chkHasGenderVariations.Location = new System.Drawing.Point(168, 56);
+            this.chkHasGenderVariations.Name = "chkHasGenderVariations";
+            this.chkHasGenderVariations.Size = new System.Drawing.Size(150, 16);
+            this.chkHasGenderVariations.TabIndex = 26;
+            this.chkHasGenderVariations.Text = "Has Gender Variations";
+            this.chkHasGenderVariations.UseVisualStyleBackColor = true;
+            // 
+            // chkCoversLegs
+            // 
+            this.chkCoversLegs.AutoSize = true;
+            this.chkCoversLegs.Location = new System.Drawing.Point(168, 285);
+            this.chkCoversLegs.Name = "chkCoversLegs";
+            this.chkCoversLegs.Size = new System.Drawing.Size(90, 16);
+            this.chkCoversLegs.TabIndex = 27;
+            this.chkCoversLegs.Text = "Covers Legs";
+            this.chkCoversLegs.UseVisualStyleBackColor = true;
+            // 
+            // chkCoversHands
+            // 
+            this.chkCoversHands.AutoSize = true;
+            this.chkCoversHands.Location = new System.Drawing.Point(168, 368);
+            this.chkCoversHands.Name = "chkCoversHands";
+            this.chkCoversHands.Size = new System.Drawing.Size(96, 16);
+            this.chkCoversHands.TabIndex = 28;
+            this.chkCoversHands.Text = "Covers Hands";
+            this.chkCoversHands.UseVisualStyleBackColor = true;
+            // 
+            // chkCoversBody
+            // 
+            this.chkCoversBody.AutoSize = true;
+            this.chkCoversBody.Location = new System.Drawing.Point(168, 442);
+            this.chkCoversBody.Name = "chkCoversBody";
+            this.chkCoversBody.Size = new System.Drawing.Size(90, 16);
+            this.chkCoversBody.TabIndex = 29;
+            this.chkCoversBody.Text = "Covers Body";
+            this.chkCoversBody.UseVisualStyleBackColor = true;
+            // 
+            // frmItemComponentArmourEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmItemComponentArmourEditor";
+            this.ClientSize = new System.Drawing.Size(469, 504);
+            this.Controls.Add(this.chkCoversBody);
+            this.Controls.Add(this.chkCoversHands);
+            this.Controls.Add(this.chkCoversLegs);
+            this.Controls.Add(this.chkHasGenderVariations);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtBodyArmour);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtArmArmour);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtLegArmour);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtMaterialType);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtModifierGroup);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBreadCoverType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHairCoverType);
+            this.Controls.Add(this.txtHeadArmour);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmItemComponentArmourEditor";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Item Component - Armour Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHeadArmour;
+        private System.Windows.Forms.TextBox txtHairCoverType;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBreadCoverType;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtModifierGroup;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMaterialType;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtLegArmour;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtArmArmour;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtBodyArmour;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.CheckBox chkHasGenderVariations;
+        private System.Windows.Forms.CheckBox chkCoversLegs;
+        private System.Windows.Forms.CheckBox chkCoversHands;
+        private System.Windows.Forms.CheckBox chkCoversBody;
     }
 }
--- Forms/frmItemComponentArmourEditor.cs
+++ Forms/frmItemComponentArmourEditor.cs
@@ -23,6 +23,47 @@ namespace ArenaModdingTool.Forms
         {
             InitializeComponent();
             this.itemArmour = itemArmour;
+
+            if (itemArmour != null)
+            {
+                txtArmArmour.Text = itemArmour.arm_armor;
+                txtBodyArmour.Text = itemArmour.body_armor;
+                txtBreadCoverType.Text = itemArmour.beard_cover_type;
+                txtHairCoverType.Text = itemArmour.hair_cover_type;
+                txtHeadArmour.Text = itemArmour.head_armor;
+                txtLegArmour.Text = itemArmour.leg_armor;
+                txtMaterialType.Text = itemArmour.material_type;
+                txtModifierGroup.Text = itemArmour.modifier_group;
+                chkCoversBody.Checked = bool.Parse(itemArmour.covers_body);
+                chkCoversHands.Checked = bool.Parse(itemArmour.covers_hands);
+                chkCoversLegs.Checked = bool.Parse(itemArmour.covers_legs);
+                chkHasGenderVariations.Checked = bool.Parse(itemArmour.has_gender_variations);
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            itemArmour = new MBItemArmour();
+            itemArmour.arm_armor = txtArmArmour.Text;
+            itemArmour.beard_cover_type = txtBreadCoverType.Text;
+            itemArmour.body_armor = txtBodyArmour.Text;
+            itemArmour.covers_body = chkCoversBody.Checked.ToString();
+            itemArmour.covers_hands = chkCoversHands.Checked.ToString();
+            itemArmour.covers_legs = chkCoversLegs.Checked.ToString();
+            itemArmour.hair_cover_type = txtHairCoverType.Text;
+            itemArmour.has_gender_variations = chkHasGenderVariations.Checked.ToString();
+            itemArmour.head_armor = txtHeadArmour.Text;
+            itemArmour.leg_armor = txtLegArmour.Text;
+            itemArmour.material_type = txtMaterialType.Text;
+            itemArmour.modifier_group = txtModifierGroup.Text;
+
+            DialogResult = DialogResult.OK;
+            Close(); ;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
